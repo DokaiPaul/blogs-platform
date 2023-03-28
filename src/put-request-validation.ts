8c7ea9dd38722d09ab1c85dd9034b4b4eba8e2ff
DokaiPaul <dokaypaul@gmail.com>
2023-03-28 21:46:35 +0300

    hope this is final for today)

--- src/put-request-validation.ts
+++ src/put-request-validation.ts
@@ -41,7 +41,7 @@ export const putRequestValidate = (body: VideoType) => {
 
         errorsArr.errorsMessages.push(error);
     }
-    const validDateFormat: RegExp = /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d).([0-5]\d+)Z$/g;
+    const validDateFormat: RegExp = /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d).([0-9]\d+)Z$/g;
     if(body.createdAt && (typeof body.createdAt !== "string" || !validDateFormat.test(body.createdAt))) {
         let error = errorMsg("createdAt");
 
