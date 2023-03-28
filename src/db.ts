92e7093494bf851a5e4e7ff2e897bd478b6b7787
DokaiPaul <dokaypaul@gmail.com>
2023-03-28 21:30:18 +0300

    fixed POST and PUT validation

--- src/db.ts
+++ src/db.ts
@@ -43,4 +43,4 @@ export type VideoType =
         availableResolutions: string[]
     }
 
-export const supportedResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1480', 'P2160'];
+export const supportedResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160'];
--- src/put-request-validation.ts
+++ src/put-request-validation.ts
@@ -1,51 +1,56 @@
 import {supportedResolutions, VideoType} from "./db";
-
-export const errorMsg = (field: string) => ({
-    "errorsMessages": [
-        {
-            "message": "An error has been happened",
-            "field": field
-        }
-    ]
-})
+type ErrorMessage = {
+    message: string,
+    field: string
+}
+type ErrorMessages = {
+    errorsMessages: ErrorMessage[]
+}
+export const errorMsg = (field: string) => {
+    return {
+        "message": "An error has been happened",
+        "field": field
+    }
+}
 export const putRequestValidate = (body: VideoType) => {
-    const errorsArr = [];
-
+    const errorsArr: ErrorMessages = {
+        "errorsMessages": []
+    };
     if (body.title && (typeof body.title !== 'string' || body.title.length > 40)) {
         let error = errorMsg("title");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
     if (body.author && (typeof body.author !== 'string' || body.author.length > 20)) {
         let error = errorMsg("author");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
     if (body.availableResolutions && (body.availableResolutions.length === 0 || !body.availableResolutions.every(v => supportedResolutions.includes(v)))) {
         let error = errorMsg("availableResolutions");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
     if(body.canBeDownloaded && typeof body.canBeDownloaded !== "boolean") {
         let error = errorMsg("canBeDownloaded");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
     if(body.minAgeRestriction && (typeof body.minAgeRestriction !== 'number' || +body.minAgeRestriction > 18  || +body.minAgeRestriction < 1)) {
         let error = errorMsg("minAgeRestriction");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
     const validDateFormat: RegExp = /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d).([0-5]\d+)Z$/g;
     if(body.createdAt && (typeof body.createdAt !== "string" || !validDateFormat.test(body.createdAt))) {
         let error = errorMsg("createdAt");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
     if(body.publicationDate && (typeof body.publicationDate !== "string" || !validDateFormat.test(body.publicationDate))) {
         let error = errorMsg("publicationDate");
 
-        errorsArr.push(error);
+        errorsArr.errorsMessages.push(error);
     }
-    return errorsArr[0];
+    return errorsArr;
 }
