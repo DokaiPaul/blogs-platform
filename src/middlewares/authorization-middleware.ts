e66860ab828dc04f61418713744b62c7e1841159
DokaiPaul <dokaypaul@gmail.com>
2023-04-10 12:14:39 +0300

    security patch. Authorization

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -4,7 +4,12 @@ import {decodeBase64} from "../utils/decoders";
 
 export const authorizationMiddleware = header('Authorization')
     .custom(value => {
-        const credentials = decodeBase64(value);
+        const values: string[] = value.split(' ');
+        const credentials = decodeBase64(values[1]);
+        const type = values[0];
+        if(type !== 'Basic') {
+            throw new Error('You do not have permissions to perform this action')
+        }
         const login = credentials[0];
         const pass = credentials[1];
 
--- src/utils/decoders.ts
+++ src/utils/decoders.ts
@@ -1,8 +1,5 @@
 export const decodeBase64 = (input: string) => {
-    const immutable = input
-    const code = immutable.split(' ')[1];
-
-    const buff = new Buffer(code, 'base64')
+    const buff = new Buffer(input, 'base64')
     const decoded = buff.toString('ascii')
     return decoded.split(':')
 }
